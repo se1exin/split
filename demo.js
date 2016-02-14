465c5274783e4d9822620fd903c653a60ab91b8e
Selexin <jacobhmorris@gmail.com>
2016-02-14 22:33:49 +1100

    Such gambling. Wow money

---
+++ demo.js
@@ -0,0 +1,45 @@
+var split = require('./split');
+
+// Based on Melbourne Cup 2015
+// http://www.races.com.au/melbourne-cup/melbourne-cup-history/facts-and-statistics/
+var split = new split.Split(100);
+
+split.addItem(1, 51);   // SNOW SKY (16)
+split.addItem(2, 19);   // CRITERION (4)
+split.addItem(3, 5);    // FAME GAME (12)
+split.addItem(4, 21);   // OUR IVANHOWE (22)
+split.addItem(5, 61);   // BIG ORANGE (23)
+split.addItem(6, 31);   // HARTNELL (17)
+split.addItem(7, 41);   // HOKKO BRAVE (20)
+split.addItem(8, 14);   // MAX DYNAMITE (2)
+split.addItem(9, 26);   // RED CADEAUX (8)
+//split.addItem(10, 6.5); // TRIP TO PARIS (14) // Favourite - Will typically mathematically distrupt the odds. Bet on it individually if you really think it has a chance..
+split.addItem(11, 21);  // WHO SHOT THEBARMAN (6)
+split.addItem(12, 34);  // SKY HUNTER (7)
+split.addItem(13, 31);  // THE OFFER (13)
+split.addItem(14, 61);  // GRAND MARSHAL (15)
+split.addItem(15, 9);   // PREFERMENT (11)
+split.addItem(16, 81);  // QUEST FOR MORE (21)
+split.addItem(17, 17);  // ALMOONQITH (10)
+split.addItem(18, 61);  // KINGFISHER (9)
+//split.addItem(19, 101); // PRINCE OF PENZANCE (1) // Outlier - removed from calculation
+split.addItem(20, 21);  // BONDI BEACH (18)
+//split.addItem(21, 101); // SERTORIUS (5) // Outlier - removed from calculation
+split.addItem(22, 21);  // THE UNITED STATES (3)
+split.addItem(23, 34);  // EXCESS KNOWLEDGE (24)
+split.addItem(24, 41);  // GUST OF WIND (19)
+
+// Perform Split! calculation
+var results = split.gamble();
+
+// Print the results
+for(var i in results) {
+  console.log('('+ results[i].id + ') Bet: $' +  results[i].bet.toFixed(2) + ', Win: $' + results[i].result.toFixed(2) + ', Less Bet: $' + results[i].resultLessBet.toFixed(2) + ', Profit: $' + results[i].profit.toFixed(2));
+}
+
+// Will the group of best actually profit??
+if(split.winning()) {
+  console.log('Congrats! All bets will win.');
+} else {
+  console.log('Bugger! Odds aren\'t in your favour.. try removing some outliers.');
+}
---
+++ split.js
@@ -0,0 +1,155 @@
+/**
+ * Split! - A simple gambling library for betting on every competitor and winning no matter what
+ * Author: selexin
+ * https://github.com/se1exin/split
+ * Version 1.0.0 - 2016/2/16
+ */
+
+/**
+ * class SplitItem
+ *  A single competitor in a gambling event
+ *  
+ * @param mixed id - A unique identifier for the competitor. E.g. '1'
+ * @param float odds - The odds for the competitor
+ */
+ function SplitItem(id, odds) {
+    this.id = id; // Unique identifier for this competitor
+    this.odds = odds; // The odds for this competitor given to you by your favourite respectable gambling vendor
+    this.bet = 0; // How $much you should bet on this competitor
+    this.result = 0; // How $much you will be paid if this competitor wins
+    this.resultLessBet = 0; // How $much you will profit from this individual bet (not including other bets)
+    this.profit = 0; // How $much you will actually profit in total from this bet
+}
+
+/**
+ * function calcBet
+ *  Calculates the current betting results for the competitor
+ *  
+ * @param float bet 
+ */
+SplitItem.prototype.calcBet = function(bet, spend) {
+    this.bet = bet;
+    this.result = this.odds * this.bet;
+    this.resultLessBet = this.result - this.bet;
+    this.profit = this.result - spend;
+};
+
+
+/**
+ * class Split
+ *  Represents a gambling event, holding all competitors and providing betting calculations for each
+ *  
+ * @param float spend - The total amount wished to be gambled on this event
+ */
+function Split(spend) {
+    this.spend = spend;
+    this.oddsTotal = 0; // Keeps track of the accumaltive odds total. Used to determine the iterations required to calculate all results 
+    this.betIncrement = 0.01; // Adjust bets with an accuracy of 1c
+    this.items = []; // Array to hold all of the competitors
+}
+
+/**
+ * function addItem
+ *  Adds a single competitor to the event. See class SplitItem for competitor implementation
+ *  
+ * @param mixed id - A unique identifier for the competitor. E.g. '1'
+ * @param float odds - The odds for the competitor
+ */
+Split.prototype.addItem = function(id, odds) {
+    this.items.push(new SplitItem(id, odds)); // Add the competitor to the event
+    this.resetSplits(); // Recalculate bet divisions
+    this.oddsTotal += odds; // Update the event's accumulative odds total
+};
+
+/**
+ * function resetSplits
+ *  Iterates every competitor in the event and calculates their starting bet amount
+ */
+Split.prototype.resetSplits = function() {
+    for(var i=0;i<this.items.length;i++) { // Loop through every competitor
+        // The initial bet is simply the total number of competitors divided by the total spend for the event
+        this.items[i].calcBet(this.spend / this.items.length, this.spend);
+    }
+};
+
+/**
+ * function highestItemIndex
+ *  Finds the competitor than that currently has the best/highest profit
+ *  
+ * @return int - The index of the highest paying competitor from the this.items array
+ */
+Split.prototype.highestItemIndex = function() {
+    highestIndex = null;
+    highestResult = 0; // Track the currently found best profit
+    for(var i=0;i<this.items.length;i++) { // Loop all competitors
+        if(this.items[i].result > highestResult) { // If this competitor has the best profit found yet..
+            highestIndex = i; // Flag the competitor as the best found yet
+            highestResult = this.items[i].result; // Update our local best profit comparison
+        }
+    }
+    return highestIndex;
+};
+
+/**
+ * function lowestItemIndex]
+ *  Finds the competitor than that currently has the worst/lowest profit
+ *  
+ * @return int - The index of the lowest paying competitor from the this.items array
+ */
+Split.prototype.lowestItemIndex = function() {
+    highestIndex = this.highestItemIndex();
+    lowestIndex = null;
+    lowestResult = this.items[highestIndex].result; // Track the currently found worst profit. Start with the current highest profiting competitor
+    for(var i=0;i<this.items.length;i++) { // Loop all competitors
+        if(this.items[i].result < lowestResult) { // If this competitor has the worst profit found yet..
+            lowestIndex = i; // Flag the competitor as the worst found yet
+            lowestResult = this.items[i].result; // Update our local best profit comparison
+        }
+    }
+    return lowestIndex;
+};
+
+/**
+ * function gamble
+ *  Magic voodoo snakecharmerthat figures out how much to bet on each competitor to get the best possible betting results
+ *  
+ * @return Array - All competitors (as class SplitItem) with their calculated betting results 
+ */
+Split.prototype.gamble = function() {
+    /**
+     * gamble() works by looping and looping and looping all competitors until it has calculated the best possible betting outcome.
+     * This is essentially achieved by moving cents ($0.01) from the least deserving competitor (that with the highest/best profit)
+     * to the most deserving competitor (that with the lowest/worst profit) over and over again until all comptitors have the equal-est
+     * distribution of the total spend.
+     */
+    
+    // How many times we need to loop - Total spend, divided by the $ amount to be moved between competitors, times the accumulative total of odds
+    var iterations = this.spend / this.betIncrement * this.oddsTotal;
+    for(var i=0; i<iterations; i++) {
+        lowestIndex = this.lowestItemIndex(); // Find the current worst competitor
+        highestIndex = this.highestItemIndex(); // Find the current best competitor
+        this.items[highestIndex].calcBet(this.items[highestIndex].bet - this.betIncrement, this.spend); // Take money from the best competitor
+        this.items[lowestIndex].calcBet(this.items[lowestIndex].bet + this.betIncrement, this.spend); // Give money to the worst competitor
+    }
+    return this.items;
+};
+
+/**
+ * function winning
+ *  Convenience function for determining if the return from betting on this event is more than the total spend
+ * 
+ * @return Boolean - True/False to if the betting results will actually make money on this event
+ */
+Split.prototype.winning = function() {
+    for(var i=0;i<this.items.length;i++) {
+        if(this.items[i].result <= this.spend) {
+            return false;
+        }
+    }
+    return true;
+};
+
+// Exports for nodejs
+module.exports = {
+    Split: Split
+};
